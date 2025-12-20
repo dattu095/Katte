10b349adf8ed88f28c5c661c2809271a9013ac25
dattu095 <dattu7334@gmail.com>
2025-12-20 17:10:36 +0530

    init: form creator

---
+++ app/static/js/utils/form.js
@@ -0,0 +1,19 @@
+export function create_form(name, placeholder, type) {
+    const form = document.createElement('form');
+    form.method = 'post';
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.name = name;
+    input.placeholder = placeholder;
+    input.required = true;
+
+    const button = document.createElement('button');
+    button.type = 'submit';
+    button.innerText = type;
+
+    form.appendChild(input);
+    form.appendChild(button)
+
+    return form
+}
