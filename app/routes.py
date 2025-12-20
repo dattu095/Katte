cfe019487a2bd44075efbd7cfdbae601264ee693
dattu095 <dattu7334@gmail.com>
2025-12-20 19:31:17 +0530

    init: lobby routes added

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ def index():
 
 @main_bp.route('/dummy', methods=['GET'])
 def dummy():
+    print(f"Name: {session.get('username', 'N/A')}, Room Id: {session.get('room_id', 'N/A')}")
     return render_template('dummy.html')
 
 @main_bp.route('/login', methods=['GET', 'POST'])
@@ -17,5 +18,23 @@ def login():
         username = request.form.get('username', '')
         if username:
             session['username'] = username
-            return redirect(url_for('main.dummy'))
+            return redirect(url_for('main.lobby'))
     return render_template('login.html')
+
+@main_bp.route('/lobby', methods=['GET', 'POST'])
+def lobby():
+    if not session.get('username', ''):
+        return redirect(url_for('main.login'))
+    
+    if request.method == 'POST':
+        room_id = request.form.get('room_id', '')
+        if room_id:
+            session['room_id'] = room_id
+            return redirect(url_for('main.dummy'))
+    return render_template('lobby.html')
+
+@main_bp.route('/create', methods=['GET'])
+def create():
+    if not session.get('username', ''):
+        return redirect(url_for('main.login'))
+    return redirect(url_for('main.dummy'))
