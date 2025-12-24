181c91dc2140501154701db16bc6dc52c397ccbf
dattu095 <dattu7334@gmail.com>
2025-12-24 20:59:04 +0530

    fix: all cards valid when you don't have the suit

--- app/static/js/game/gameplay/hand.js
+++ app/static/js/game/gameplay/hand.js
@@ -7,7 +7,7 @@
  * @returns {HTMLDivElement}
  */
 
-import { valid_card, hand_has_ace } from "./rules.js";
+import { valid_card, hand_has_ace, hand_has_suit } from "./rules.js";
 
 
 export function create_hand(isturn, leading_suit, hand, play_card) {
@@ -22,7 +22,7 @@ export function create_hand(isturn, leading_suit, hand, play_card) {
         const c = document.createElement('button');
         c.type = "button";
         c.innerText = `${card.value_name} of ${card.suit_name}`;
-        c.disabled = (!valid_card(isturn, leading_suit, card, hand_has_ace(isturn, hand)));
+        c.disabled = (!valid_card(isturn, leading_suit, card, hand_has_ace(isturn, hand), hand_has_suit(hand, leading_suit)));
         c.onclick = (event) => {
             event.preventDefault();
             play_card(card.value, card.suit);
--- app/static/js/game/gameplay/rules.js
+++ app/static/js/game/gameplay/rules.js
@@ -8,13 +8,15 @@
  */
 
 
-export function valid_card(isturn, leading_suit, card, hand_has_ace) {
+export function valid_card(isturn, leading_suit, card, hand_has_ace, hand_has_suit) {
     if (!isturn) return false;
 
-    if (hand_has_ace) {
-        return card.value === 14 && card.suit === 1;
+    if (hand_has_ace) { 
+        return card.value === 14 && card.suit === 1; 
     }
     
+    if (!hand_has_suit) return true;
+
     if (leading_suit === 0) {
         return true;
     } else {
@@ -26,4 +28,8 @@ export function hand_has_ace(isturn, hand) {
     if (!isturn) return false;
 
     return hand.some(card => card.value === 14 && card.suit === 1);
+}
+
+export function hand_has_suit(hand, suit) {
+    return hand.some(card => card.suit === suit);
 }
